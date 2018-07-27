6fc298fc3953794041985e95f27304c495a4bb62
Hendi Santika <hendisantika@gmail.com>
2018-07-28 04:23:40 +0700

    #8 - Add Unit Test

--- src/test/java/com/hendisantika/springbootaudit/SpringBootAuditApplicationTests.java
+++ src/test/java/com/hendisantika/springbootaudit/SpringBootAuditApplicationTests.java
@@ -1,16 +1,68 @@
 package com.hendisantika.springbootaudit;
 
+import com.hendisantika.springbootaudit.domain.User;
+import com.hendisantika.springbootaudit.repository.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootAuditApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    @Before
+    public void create() {
+        user = userRepository.save(
+                new User().setName("Uzumaki Naruto").setUsername("naruto")
+        );
+
+        assertThat(user.getCreated())
+                .isNotNull();
+
+        assertThat(user.getModified())
+                .isNotNull();
+
+        assertThat(user.getCreatedBy())
+                .isEqualTo("Mr. Auditor");
+
+        assertThat(user.getModifiedBy())
+                .isEqualTo("Mr. Auditor");
+    }
+
     @Test
-    public void contextLoads() {
+    public void update() {
+        LocalDateTime created = user.getCreated();
+        LocalDateTime modified = user.getModified();
+
+        userRepository.save(
+                user.setUsername("naruto")
+        );
+
+        userRepository.findById(user.getId())
+                .ifPresent(updatedUser -> {
+
+                    assertThat(updatedUser.getUsername())
+                            .isEqualTo("naruto");
+
+                    assertThat(updatedUser.getCreated())
+                            .isEqualTo(created);
+
+                    assertThat(updatedUser.getModified())
+                            .isAfter(modified);
+                });
     }
 
 }
